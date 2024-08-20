2ccfb318c92b1a9ae3f17544eeb8a38af207795d
coolyash678 <srivastavayash455@gmail.com>
2024-08-20 11:26:39 -0400

    Initial commit

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,73 +1,90 @@
-// Import necessary modules from React library
-import React, { useEffect } from 'react';
-
-// Import components for routing from react-router-dom library
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Import custom Navbar component
-import Navbar from './Components/Navbar/Navbar';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
-// Function component for the main App
-function App() {
 
-  // Render the main App component
-  return (
-    <>
-    <div className="App">
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [, setEmail] = useState(""); // This state is used to store the email address
+
+    const handleClick = () => setClick(!click);
+
+    const handleLogout = () => {
+        sessionStorage.removeItem("auth-token");
+        sessionStorage.removeItem("name");
+        sessionStorage.removeItem("email");
+        sessionStorage.removeItem("phone");
+        localStorage.removeItem("doctorData");
+        setIsLoggedIn(false);
+        setEmail(''); // Clear the email state
+        // Remove the reviewFormData from local storage
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            if (key.startsWith("reviewFormData_")) {
+                localStorage.removeItem(key);
+            }
+        }
+        window.location.reload();
+    };
+
+    useEffect(() => { 
+        const storedEmail = sessionStorage.getItem("email");
+
+        if (storedEmail) {
+            setIsLoggedIn(true);
+            setEmail(storedEmail);
+        }
+    }, []);
+
+    return (
         <nav>
-        <div class="nav__logo">
-          <a href="/">
-            StayHealthy 
-            <svg xmlns="http://www.w3.org/2000/svg" height="26" width="26" viewBox="0 0 1000 1000" style="fill:#3685fb">
-                <title>Doctor With Stethoscope SVG icon</title>
-                <g>
-                    <g>
-                        <path d="M499.8,10c91.7,0,166,74.3,166,166c0,91.7-74.3,166-166,166c-91.7,0-166-74.3-166-166C333.8,84.3,408.1,10,499.8,10z"></path>
-                        <path d="M499.8,522.8c71.2,0,129.1-58.7,129.1-129.1H370.6C370.6,464.1,428.6,522.8,499.8,522.8z"></path>
-                        <path d="M693.2,395c-0.7,94.9-70.3,173.7-160.8,188.9v155.9c0,80.3-60.7,150.8-140.8,155.3c-83,4.7-152.7-58.9-157.6-139.7c-22-12.8-35.6-38.5-30.3-66.7c4.7-25.1,25.5-45.6,50.8-49.9c39.7-6.7,74.1,23.7,74.1,62.1c0,23-12.3,43-30.7,54.1c4.7,45.4,45.1,80.4,92.6,76c44.6-4,77.2-44...."></path>
-                    </g>
-                </g>
-            </svg>
-          </a>
-          <span>.</span>
-        </div>
-        <div class="nav__icon" onClick={handleClick}>
-          <i class="fa fa-times fa fa-bars"></i>
-        </div>
-        <ul class="nav__links active">
-          <li class="link">
-            <a href="../Landing_Page/LandingPage.html">Home</a>
-          </li>
-          <li class="link">
-            <a href="#">Appointments</a>
-          </li>
-          <li class="link">
-            <a href="../Signup//Signup.html">
-              <button class="btn1">Sign Up</button>
-            </a>
-          </li>
-          <li class="link">
-            <a href="../Login/Login.html">
-              <button class="btn1">Login</button>
-            </a>
-          </li>
-        </ul>
-    </nav>
-    </>
-        {/* Set up BrowserRouter for routing */}
-        <BrowserRouter>
-          {/* Display the Navbar component */
-          }
-          <Navbar/>
+            <div className="nav__logo">
+                <Link to="/">
+                    StayHealthy <i style={{color:'#2190FF'}} className="fa fa-user-md"></i>
+                </Link>
+                <span>.</span>
+            </div>
+            <div className="nav__icon" onClick={handleClick}>
+                <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+            </div>
+            <ul className={click ? 'nav__links active' : 'nav__links'}>
+                <li className="link">
+                    <Link to="/landingpage">Home</Link>
+                </li>
+                <li className="link">
+                    <Link to="/search/doctors">Appointments</Link>
+                </li>
+                <li className="link">
+                    <Link to="/healthblog">Health Blog</Link>
+                </li>
+                <li className="link">
+                    <Link to="/reviews">Reviews</Link>
+                </li>
+                {isLoggedIn ? (
+                    <>
+                        <li className="link">
+                            <button className="btn2" onClick={handleLogout}>
+                                Logout
+                            </button>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li className="link">
+                            <Link to="/signup">
+                                <button className="btn1">Sign Up</button>
+                            </Link>
+                        </li>
+                        <li className="link">
+                            <Link to="/login">
+                                <button className="btn1">Login</button>
+                            </Link>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </nav>
+    );
+};
 
-          {/* Set up the Routes for different pages */}
-          <Routes>
-            {/* Define individual Route components for different pages */}
-          </Routes>
-        </BrowserRouter>
-    </div>
-  );
-}
-
-// Export the App component as the default export
-export default App;
+export default Navbar;
